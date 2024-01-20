11e4e30207c863289e5b11437b9735e68ffb64dc
Bad Madafucker <badmadafucker@bads-mbp.lan>
2024-01-20 22:48:39 +1300

    added TEST COUNT ENV VAR

--- src/test_ft_get_file_extension.py
+++ src/test_ft_get_file_extension.py
@@ -1,10 +1,13 @@
 from hypothesis.strategies import text
 from hypothesis import given, settings
 import ft_get_file_extension
+import os
+
+test_count = int(os.environ["TEST_COUNT"])
 
 @given(name=text(),
        extension=text(alphabet='abcdefghijklmnopqrstuvwxyz'))
-@settings(max_examples=3333)
+@settings(max_examples=test_count)
 def test_get_file_extension(name, extension):
     file_name = name + "." + extension
     extension = ft_get_file_extension.get_file_extension(file_name)
